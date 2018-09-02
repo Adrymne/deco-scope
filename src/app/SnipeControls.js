4fa3969116af9993120493ba780f14f17b6ab954
Adrymne <jealousy.of.tomorrow@gmail.com>
2018-09-02 17:14:27 +1000

    Implement Snipe Controls behaviour

--- src/app/SnipeControls.js
+++ src/app/SnipeControls.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Row, Col, Button } from 'reactstrap';
+import { connect } from 'react-redux';
+import * as actions from 'store/actions';
 
-const SnipeControls = () => (
+const SnipeControls = ({ doMeld, doQuest }) => (
   <Row>
     <Col xs="6">
-      <Button block color="success">
+      <Button block color="success" onClick={doMeld}>
         Meld
       </Button>
     </Col>
     <Col xs="6">
-      <Button block color="warning">
+      <Button block color="warning" onClick={doQuest}>
         Quest
       </Button>
     </Col>
   </Row>
 );
 
-export default SnipeControls;
+export default connect(
+  undefined,
+  actions
+)(SnipeControls);
